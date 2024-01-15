7bc3b18226b89983a0e24da7cd41a1499984bda5
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-01-15 20:25:21 -0300

    feat: initial addition of the login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    AuthFormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/auth-form/auth-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.scss']
+})
+export class AuthFormComponent {
+
+  @Input() type: string = '';
+
+}
