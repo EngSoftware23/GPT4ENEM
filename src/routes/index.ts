959921cd27bbf6bf50549facbe9ef9dd7573337e
Mateus da Silva <mso3@cin.ufpe.br>
2024-03-09 22:33:26 -0300

    added package-lock.json, package.json

---
+++ src/routes/index.ts
@@ -0,0 +1,20 @@
+import express,{Router, Request, Response} from 'express';
+
+const mainRouter = Router();
+
+mainRouter.get('/login', (req: Request, res: Response) => {
+});
+
+mainRouter.get('/registro', (req: Request, res: Response) => {
+});
+
+mainRouter.get('/historico', (req: Request, res: Response) => {
+});
+
+mainRouter.get('/servico', (req: Request, res: Response) => {
+});
+
+mainRouter.get('/analise', (req: Request, res: Response) => {
+});
+
+export default mainRouter;
