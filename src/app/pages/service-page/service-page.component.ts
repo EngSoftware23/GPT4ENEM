2eb3a8ffcf0d0e8e47dde4b1eb99e4b5969684c8
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-03-15 17:10:23 -0300

    feat: updating angular 16 to 17 and adding the possibility of adding animations from the lib lottie files

--- src/app/pages/service-page/service-page.component.ts
+++ src/app/pages/service-page/service-page.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-service-page',
   templateUrl: './service-page.component.html',
   styleUrls: ['./service-page.component.scss']
 })
-export class ServicePageComponent {
+export class ServicePageComponent   {
+  serviceForm: FormGroup;
+  videoId:string | null = '';
+  displayLoading:boolean = true;
+  textsLoading: string[] = ['Espere um momento 😀', 'Assistindo a Aula 🧐', 'Aprendendo o Assunto 🫡', 'Criando o Resumo 😎', 'Enviando pra Você ✅']
+  textDisplayIndex: number = 0;
+  textDisplay: string = this.textsLoading[this.textDisplayIndex]
 
+  constructor(private fb: FormBuilder) { 
+    this.serviceForm = this.fb.group({
+      linkUrl: ['', [Validators.required]]
+    });
+  }
+
+
+ 
+  selectedItemIndex: number | null = null;
+
+  onItemClick(index: number): void {
+    this.selectedItemIndex = index;
+  }
+
+  inviteData() {
+    console.log(this.selectedItemIndex)
+    console.log(this.serviceForm.value.linkUrl);
+    let linkUrl = this.serviceForm.value.linkUrl;
+    this.videoId =  this.extrairIdDoVideo(linkUrl)
+    console.log(this.videoId)
+    this.displayLoading = true;
+
+    setTimeout(() => {
+      window.scrollBy({
+        top: window.innerHeight, 
+        behavior: 'smooth' 
+      });
+    }, 500)
+
+    this.changeTextLoading()
+    
+  }
+
+  extrairIdDoVideo(linkUrl: string): string | null {
+    if (linkUrl.includes('https://www.youtube.com/watch?v=')) {
+        const startIndex = linkUrl.indexOf('v=') + 2; 
+        const endIndex = linkUrl.indexOf('&', startIndex); 
+
+        const idEndIndex = endIndex !== -1 ? endIndex : linkUrl.length;
+
+        const videoId = linkUrl.substring(startIndex, idEndIndex);
+        return videoId;
+    } else if (linkUrl.includes('https://youtu.be/')) {
+        const startIndex = linkUrl.indexOf('.be/') + 4; 
+        const endIndex = linkUrl.indexOf('?', startIndex); 
+
+        const idEndIndex = endIndex !== -1 ? endIndex : linkUrl.length;
+
+        const videoId = linkUrl.substring(startIndex, idEndIndex);
+        return videoId;
+    }
+    
+    else {
+        return null; 
+    }
+  }
+
+  changeTextLoading():void {
+    setInterval(() => {
+      this.textDisplayIndex = (this.textDisplayIndex + 1) % this.textsLoading.length;
+      this.textDisplay = this.textsLoading[this.textDisplayIndex];
+    }, 5000);
+  }
+
+  
 }
--- src/app/pages/service-page/service-page.module.ts
+++ src/app/pages/service-page/service-page.module.ts
@@ -1,19 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicePageComponent } from './service-page.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
-import { ServiceFormComponent } from './sections/service-form/service-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ServiceResultComponent } from './sections/service-result/service-result.component';
-
 
 
 @NgModule({
   declarations: [
     ServicePageComponent,
-    ServiceFormComponent,
-    ServiceResultComponent
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     CommonModule,
     LayoutModule,
