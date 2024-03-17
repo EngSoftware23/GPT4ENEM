5910c4bae4fc4f2cafac455c521f3d9c6cac7377
Mateus da Silva <mso3@cin.ufpe.br>
2024-03-17 02:10:49 -0300

    Added dynamic prompt for the summary and content verification services of ENEM.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,5 @@
 import express, { Router, Request, Response } from 'express';
 
-import admin from "../services/serviceFirebase";
-
-const db = admin.firestore();
-
 const downloadController = require('../controllers/downloadController');
 
 const mainRouter = Router();
@@ -11,16 +7,13 @@ const mainRouter = Router();
 mainRouter.get('/', (req: Request, res: Response) => {
 });
 
-mainRouter.get('/login/historico', async (req: Request, res: Response) => {});
+mainRouter.get('/historico', async (req: Request, res: Response) => {});
 
-
-mainRouter.get('/login/servico', (req: Request, res: Response) => {
-});
-
-mainRouter.get('/login/analise', (req: Request, res: Response) => {
-});
+mainRouter.post('/servicos', downloadController.downloadAudio)
 
 mainRouter.post('/transcricao', downloadController.downloadAudio)
+
+mainRouter.post('/resumo', downloadController.downloadAudio)
 
 mainRouter.post('/revisao', downloadController.downloadAudio)
 
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express, { Express } from "express";
 import mainRouter from "./routes";
-import { createTestData } from './database/firestoreDatabase'; 
-import OpenAI from 'openai';
-
 
 
 const app: Express = express();
-
-app.use(express.json());
 
 
 app.use(mainRouter);
 
 app.listen(80, () => {
     console.log('Aplicação rodando na porta 80');
-    //createTestData().catch(console.error); 
 });
