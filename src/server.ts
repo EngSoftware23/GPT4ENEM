048e2f8cee0a2305ad9ee6c06779a39fca9953e2
Mateus da Silva <mso3@cin.ufpe.br>
2024-03-17 16:51:34 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import mainRouter from "./routes";
+import cors from "cors";
 
 
 const app: Express = express();
 
 app.use(mainRouter);
+app.use(cors()); 
 
 app.listen(80, () => {
     console.log('Aplicação rodando na porta 80');
