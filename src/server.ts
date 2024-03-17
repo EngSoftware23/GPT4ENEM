81e2666cdb4d09e27d139e7e0338648c28caefa3
Mateus da Silva <mso3@cin.ufpe.br>
2024-03-17 16:54:42 -0300

    correction import from router

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { Express } from "express";
-import mainRouter from "./routes";
-import cors from "cors";
+import mainRouter from "./routes/index";
+import cors from "cors"; // Importe o módulo cors
 
 
 const app: Express = express();
 
 app.use(mainRouter);
-app.use(cors()); 
+app.use(cors()); // Use o middleware cors para todas as rotas
 
 app.listen(80, () => {
     console.log('Aplicação rodando na porta 80');
